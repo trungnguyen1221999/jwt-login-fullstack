e9474ac5f6bc10ddc49641e1c5b6a60a42aeddde
Trung Nguyen <trungnguyen1221999@gmail.com>
2025-10-20 20:05:04 +0300

    config be

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 
 import express from 'express'
-import dotnet from 'dotnet'
-dotnet.config();
+import dotenv from 'dotenv'
+dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 5002;
