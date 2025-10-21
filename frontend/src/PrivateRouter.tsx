ff21d8db40f347f4727b08bf0228a7bbb3b7ae00
Trung Nguyen <trungnguyen1221999@gmail.com>
2025-10-21 13:17:04 +0300

    Delete frontend directory

--- frontend/src/PrivateRouter.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRouter = () => {
-  const token = localStorage.getItem("accessToken");
-  return token ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default PrivateRouter;
